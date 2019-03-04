2056c8ffe346456a0214d0d6ea668762c7078126
ak2hr <ak2hr@virginia.edu>
2019-03-03 23:55:15 -0500

    condensed to one file

--- Capstone/display_reading.php
+++
@@ -1,3 +0,0 @@
-$my_bucket = us.artifacts.academic-veld-231919.appspot.com
-$fileContents = file_get_contents("gs://${my_bucket}/mostRecent.txt");
-echo($fileContents);
--- Capstone/receive_things_data.php
+++ Capstone/receive_things_data.php
@@ -1,6 +1,19 @@
-$my_bucket = us.artifacts.academic-veld-231919.appspot.com
-$request_body = file_get_contents('php://input');
-$data = json_decode($request_body);
-$fp = fopen("gs://${my_bucket}/mostRecent.txt", "w");
-fwrite($fp, $data);
-fclose($fp);
+<?php
+
+$my_bucket = "us.artifacts.academic-veld-231919.appspot.com";
+
+if($_GET['type'] === "write") {
+	
+	$request_body = file_get_contents('php://input');
+	$data = json_decode($request_body);
+	$fp = fopen("gs://${my_bucket}/mostRecent.txt", "w");
+	fwrite($fp, $data);
+	fclose($fp);
+}
+
+if($_GET['type'] === "read") {	
+	$fileContents = file_get_contents("gs://${my_bucket}/mostRecent.txt");
+	echo($fileContents);
+}
+
+?>
